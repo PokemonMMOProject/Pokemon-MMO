cea4f5a4c6ff8eff962dbb7d17f861c1fabb459c
Rainzye <Rainzye@users.noreply.github.com>
2016-10-02 16:17:06 +0100

    totally editing server.cpp to add settings + SQL database stuff (brain = in pain)

--- src/Server/server.cpp
+++ src/Server/server.cpp
@@ -75,4 +75,43 @@ void Server::start(){
         }
     };
 
-    // I need a break I will continue in a few hours - Dont edit.
+    // Hopefully I won't get caught by the WoW R.I.O.T Police Force for this edit to server.cpp
+    setDefaultValue("Scripts/SafeMode", false); // All this does is useless crap like stopping you from giving someone owner, or a shutdown command etc.
+    setDefaultValue("Server/Password", "123456"); // Best password for server default ik.
+    setDefaultValue("Server/RequirePassword", false); // False because we dont want to use a password rn
+    setDefaultValue("Server/Private", false); // Its a public server, So this setting is false
+    setDefaultValue("Server/Name", QString()); // Pick a server name from GUI
+    setDefaultValue("Server/Announcement", QString()); // An announcement which will show when someone logs in
+    setDefaultValue("Server/MaxPlayers", 0); // 0 = Infinite Slots (Can be changed in GUI
+    setDefaultValue("Network/ProxyServers",QString("127.0.0.1,::1%0,localhost")); // Localhost isn't a proxy but messes up internal engine for being a bad IP range...
+    setDefaultValue("Network/LowTCPDelay", false); // Sacrifice bandwidth for latency?
+    setDefaultValue("AntiDOS/ShowOveractiveMessages", true); // Shows the IPs which spam in chat to all
+    setDefaultValue("AntiDOS/TrustedIps", "127.0.0.1,::1%0,localhost"); // These IPs cannot be banned, obv localhost.
+    setDefaultValue("AntiDOS/MaxPeoplePerIp", 1); // Since when do we allow TWO people per HOUSE???
+    setDefaultValue("AntiDOS/MaxCommandsPerUser", 50); // Doubt someone would spam 50 commands per minute
+    setDefaultValue("AntiDOS/MaxKBPerUser", 25); // You go over this limit per minute you get kicked out
+    setDefaultValue("AntiDOS/MaxConnectionRatePerIP", 6); // Not a clue but needed anyways..
+    setDefaultValue("AntiDOS/NumberOfInfractionsBeforeBan", 10); // Bans someone if they spam 10 times over 25 KB per minute
+    setDefaultValue("AntiDOS/Disabled", false); // As if I'd disable such a system?
+    setDefaultValue("Registry/IP", "some-domain-or-ip-goes.here"); // Some registry for servers to use or something (OVH recommended)
+
+    // Oh no...I feel the pain...SQL is getting closer!
+    setDefaultValue("SQL/Driver", SQLCreator::SQLite);
+    setDefaultValue("SQL/Database", "default"); // Blah blah blah database name blah blah
+    setDefaultValue("SQL/Port", 5432);
+    setDefaultValue("SQL/User", "postgres");
+    setDefaultValue("SQL/Pass", "default");
+    setDefaultValue("SQL/Host", "localhost"); // Keep database local else people would hack it.
+    setDefaultValue("SQL/DatabaseSchema", ""); // For different types of databases
+
+    registry = new RegistryCommunicator(s.value("Registry/IP").toString(), this); // Needs domain/IP
+
+    if (isSql()) {
+        try {
+            SQLCreator::createSQLConnection();
+        } catch (const QString &ex) {
+            forcePrint(ex);
+        }
+    }
+
+    // Im done SQL hurts my brain will continue soon
